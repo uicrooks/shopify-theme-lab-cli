2b5774bbdecb4eb9f03d5d6cf2bc940da802d0c4
Sergej Samsonenko <contact@sergej.codes>
2020-12-22 13:58:36 +0100

    added theme command

--- bin/themelab.js
+++ bin/themelab.js
@@ -22,6 +22,13 @@ program
     require('../lib/create')(projectName)
   })
 
+program
+  .command('theme <repository>')
+  .description('setup a theme from a GitHub repository')
+  .action((repository) => {
+    require('../lib/theme')(repository)
+  })
+
 /**
  * arguments
  */
