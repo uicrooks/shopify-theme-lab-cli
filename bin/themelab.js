4371725d11e38e4d7d0d84aa95725676fc7f3d5b
Sergej Samsonenko <contact@sergej.codes>
2020-12-24 19:56:01 +0100

    added theme initialization

--- bin/themelab.js
+++ bin/themelab.js
@@ -4,12 +4,12 @@
  * require
  */
 const { program } = require('commander')
-const minimist = require('minimist')
 
 /**
  * setup
  */
 program
+  .storeOptionsAsProperties(false)
   .version(`Shopify Theme Lab CLI ${require('../package').version}`, '-v, --version', 'output the version number')
   .usage('<command> [options]')
 
@@ -20,8 +20,19 @@ program
   .command('create <directory>')
   .description('create a Shopify Theme Lab environment')
   .option('-r, --repo <url>', 'use a git repository url')
-  .action((directory) => {
-    require('../lib/create')(directory, minimist(process.argv.slice(3)))
+  .action((directory, cmd) => {
+    require('../lib/create')(directory, cmd)
+  })
+
+program
+  .command('shopify:init')
+  .description('initialize theme on remote Shopify store and a local config')
+  .requiredOption('-p, --password <password>', 'Shopify plugin API password')
+  .requiredOption('-s, --store <store>', '[your-store].myshopify.com')
+  .option('-e, --env <env>', 'Environment [dev] or [live]', 'dev')
+  .option('-n, --name <name>', 'Theme name', 'Shopify Theme Lab')
+  .action((cmd) => {
+    require('../lib/shopify-init')(cmd)
   })
 
 /**
