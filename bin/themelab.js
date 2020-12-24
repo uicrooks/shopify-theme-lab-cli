5e3e25bb8cb224aef01bf33f621e469407801f53
Sergej Samsonenko <contact@sergej.codes>
2020-12-24 21:39:39 +0100

    added option to initialize local config only

--- bin/themelab.js
+++ bin/themelab.js
@@ -31,6 +31,7 @@ program
   .requiredOption('-s, --store <store>', '[your-store].myshopify.com')
   .option('-e, --env <env>', 'Environment [dev] or [live]', 'dev')
   .option('-n, --name <name>', 'Theme name', 'Shopify Theme Lab')
+  .option('-i, --id <id>', 'If you provide a theme id, the command only creates a local config')
   .action((cmd) => {
     require('../lib/shopify-init')(cmd)
   })
